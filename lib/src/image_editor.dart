e61d11d6af7f7bf0957cf8c10f0d270add2466e8
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-05 19:46:17 +0200

    delete unuseful function

--- lib/src/image_editor.dart
+++ lib/src/image_editor.dart
@@ -382,3 +382,15 @@ class SecondScreen extends StatelessWidget {
     );
   }
 }
+
+///The [Filter] class to define a Filter consists of multiple [SubFilter]s
+abstract class Filter extends Object {
+  final String name;
+  Filter({required this.name});
+
+  ///Apply the [SubFilter] to an Image.
+  void apply(Uint8List pixels, int width, int height);
+}
+
+///The [SubFilter] class is the abstract class to define any SubFilter.
+abstract class SubFilter extends Object {}
