29c1ca5ee467b52e7bc45effa5237d7989f422cc
Ahmed Abdo <ahmedabdoelhawari12345work@gmail.com>
2022-08-07 09:50:23 +0200

    refactoring the code

--- lib/custom_gallery_display.dart
+++ lib/custom_gallery_display.dart
@@ -1,8 +1,8 @@
 library custom_gallery_display;
 
 export 'src/custom_gallery_display.dart';
-export 'src/selected_image_details.dart';
+export 'src/entities/selected_image_details.dart';
 export 'src/camera_display.dart';
-export 'src/tabs_texts.dart';
-export 'src/app_theme.dart';
+export 'src/entities/tabs_texts.dart';
+export 'src/entities/app_theme.dart';
 export 'src/permissions.dart';
