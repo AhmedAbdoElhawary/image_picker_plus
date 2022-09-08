1b769dca940e87c108c75900ac19c755e561c2d9
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-09-08 20:07:52 +0200

    add CustomTwoAsyncValueSetter

--- lib/src/utilities/typedef.dart
+++ lib/src/utilities/typedef.dart
@@ -1,1 +1,2 @@
-typedef CustomAsyncValueSetter<A, B, C> = A Function(B value, C value2);
+typedef CustomThreeAsyncValueSetter<A, B, C> = A Function(B value, C value2);
+typedef CustomTwoAsyncValueSetter<A, B> = Future<A> Function(B value);
