b055f84a7f122f83025806fb7b6294faafada150
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-09-08 20:07:15 +0200

    change the name of custom_gallery_display to image_picker_plus

--- lib/image_picker_plus.dart
+++ lib/image_picker_plus.dart
@@ -1,8 +1,9 @@
-library custom_gallery_display;
+library image_picker_plus;
 
-export 'src/custom_gallery_display.dart';
+export 'src/entities/image_picker_display.dart';
+export 'src/utilities/enum_image_source.dart';
+export 'src/image_picker_plus.dart';
 export 'src/entities/selected_image_details.dart';
-export 'src/camera_display.dart';
 export 'src/entities/tabs_texts.dart';
 export 'src/entities/app_theme.dart';
 export 'src/permissions.dart';
