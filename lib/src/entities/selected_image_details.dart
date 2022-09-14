d9d9c75d5e6f820082a89b7d391fe0858222845d
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-09-14 19:54:34 +0200

    reformat the code

--- lib/src/entities/selected_image_details.dart
+++ lib/src/entities/selected_image_details.dart
@@ -1,19 +1,27 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class SelectedImagesDetails {
-  List<SelectedByte> selectedBytes;
+  List<SelectedByte> selectedFiles;
   double aspectRatio;
   bool multiSelectionMode;
 
   SelectedImagesDetails({
-    required this.selectedBytes,
+    required this.selectedFiles,
     required this.aspectRatio,
     required this.multiSelectionMode,
   });
 }
 
 class SelectedByte {
-  File selectedByte;
+  File selectedFile;
+  Uint8List selectedByte;
+
   bool isThatImage;
-  SelectedByte({required this.isThatImage, required this.selectedByte});
+  SelectedByte({
+    required this.isThatImage,
+    required this.selectedFile,
+    required this.selectedByte,
+  });
 }
