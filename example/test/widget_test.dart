839548ce662d7aa373ac5b4fbd965ce801d35dc7
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-07-28 07:36:28 -0700

    reformat the code

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:example_image_picker_plus/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
