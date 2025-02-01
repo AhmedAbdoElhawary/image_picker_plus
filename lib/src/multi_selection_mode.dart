092d176ee1ac1e481b0666eab10b1dfc23797a19
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-02-01 11:25:40 +0200

    refactoring MultiSelectionMode

--- lib/src/multi_selection_mode.dart
+++ lib/src/multi_selection_mode.dart
@@ -9,12 +9,12 @@ class MultiSelectionMode extends StatelessWidget {
 
   final File image;
   const MultiSelectionMode({
-    Key? key,
+    super.key,
     required this.image,
     required this.imageSelected,
     required this.multiSelectedImage,
     required this.multiSelectionMode,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -22,31 +22,29 @@ class MultiSelectionMode extends StatelessWidget {
       valueListenable: multiSelectionMode,
       builder: (context, bool multiSelectionModeValue, child) => Visibility(
         visible: multiSelectionModeValue,
-        child: Align(
-          alignment: Alignment.topRight,
-          child: Padding(
-            padding: const EdgeInsets.all(3),
-            child: Container(
-              height: 25,
-              width: 25,
-              decoration: BoxDecoration(
-                color: imageSelected
-                    ? Colors.blue
-                    : const Color.fromARGB(115, 222, 222, 222),
-                border: Border.all(
-                  color: Colors.white,
-                ),
-                shape: BoxShape.circle,
-              ),
-              child: imageSelected
-                  ? Center(
-                      child: Text(
-                        "${multiSelectedImage.indexOf(image) + 1}",
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    )
-                  : Container(),
+        child: child ?? const SizedBox(),
+      ),
+      child: Align(
+        alignment: Alignment.topRight,
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: Container(
+            height: 25,
+            width: 25,
+            decoration: BoxDecoration(
+              color: imageSelected ? Colors.blue : const Color.fromARGB(115, 222, 222, 222),
+              border: Border.all(color: Colors.white),
+              shape: BoxShape.circle,
             ),
+            child: imageSelected
+                ? Center(
+                    key: ValueKey("text:${multiSelectedImage.indexOf(image) + 1}"),
+                    child: Text(
+                      "${multiSelectedImage.indexOf(image) + 1}",
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  )
+                : const SizedBox(),
           ),
         ),
       ),
