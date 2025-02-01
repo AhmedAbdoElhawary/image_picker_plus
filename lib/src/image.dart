026fe7877ebd570ab18cec417ef0e85e45c3f92e
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-02-01 11:25:54 +0200

    refactoring MemoryImageDisplay

--- lib/src/image.dart
+++ lib/src/image.dart
@@ -2,47 +2,37 @@ import 'dart:typed_data';
 import 'package:image_picker_plus/image_picker_plus.dart';
 import 'package:flutter/material.dart';
 
-class MemoryImageDisplay extends StatefulWidget {
+class MemoryImageDisplay extends StatelessWidget {
   final Uint8List imageBytes;
   final AppTheme appTheme;
 
-  const MemoryImageDisplay(
-      {Key? key, required this.imageBytes, required this.appTheme})
-      : super(key: key);
-
-  @override
-  State<MemoryImageDisplay> createState() => _NetworkImageDisplayState();
-}
-
-class _NetworkImageDisplayState extends State<MemoryImageDisplay> {
-  @override
-  void didChangeDependencies() {
-    precacheImage(MemoryImage(widget.imageBytes), context);
-    super.didChangeDependencies();
-  }
+  const MemoryImageDisplay({super.key, required this.imageBytes, required this.appTheme});
 
   @override
   Widget build(BuildContext context) {
-    return buildOctoImage();
-  }
+    precacheImage(MemoryImage(imageBytes), context);
 
-  Widget buildOctoImage() {
     return Container(
       width: double.infinity,
-      color: widget.appTheme.shimmerBaseColor,
+      color: appTheme.shimmerBaseColor,
       child: Image.memory(
-        widget.imageBytes,
-        errorBuilder: (context, url, error) => buildError(),
+        imageBytes,
+        errorBuilder: (context, url, error) => _ErrorWidget(widget: this),
         fit: BoxFit.cover,
         width: double.infinity,
       ),
     );
   }
+}
 
-  SizedBox buildError() {
+class _ErrorWidget extends StatelessWidget {
+  const _ErrorWidget({required this.widget});
+
+  final MemoryImageDisplay widget;
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
-        width: double.infinity,
-        child: Icon(Icons.warning_amber_rounded,
-            color: widget.appTheme.focusColor));
+        width: double.infinity, child: Icon(Icons.warning_amber_rounded, color: widget.appTheme.focusColor));
   }
 }
