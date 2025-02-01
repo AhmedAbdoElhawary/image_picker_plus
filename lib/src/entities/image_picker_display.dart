2b53d9ab3176b614905a33f95e3f9fad200fc3e9
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-02-01 11:25:35 +0200

    refactoring GalleryDisplaySettings

--- lib/src/entities/image_picker_display.dart
+++ lib/src/entities/image_picker_display.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/foundation.dart';
 import 'package:image_picker_plus/image_picker_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker_plus/src/crop_image/crop_image.dart';
 
 /// [GalleryDisplaySettings] When you make ImageSource from the camera these settings will be disabled because they belong to the gallery.
 class GalleryDisplaySettings {
-  AppTheme? appTheme;
-  TabsTexts? tabsTexts;
-  SliverGridDelegateWithFixedCrossAxisCount gridDelegate;
-  bool showImagePreview;
-  int maximumSelection;
+  final AppTheme? appTheme;
+  final TabsTexts? tabsTexts;
+  final SliverGridDelegateWithFixedCrossAxisCount gridDelegate;
+  final bool showImagePreview;
+  final int maximumSelection;
   final AsyncValueSetter<SelectedImagesDetails>? callbackFunction;
+  final CropEditImageType cropEditImageType;
 
   /// If [cropImage] true [showImagePreview] will be true
   /// Right now this package not support crop video
-  bool cropImage;
+  final bool cropImage;
 
   GalleryDisplaySettings({
     this.appTheme,
     this.tabsTexts,
     this.callbackFunction,
+    this.cropEditImageType = CropEditImageType.normal,
     this.gridDelegate = const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 4, crossAxisSpacing: 1.7, mainAxisSpacing: 1.5),
     this.showImagePreview = false,
