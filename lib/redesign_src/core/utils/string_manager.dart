24f0d5d4a4806d75ce2596c3adee5e1c53be1010
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-02-24 23:26:43 +0200

    create StringsManager

---
+++ lib/redesign_src/core/utils/string_manager.dart
@@ -0,0 +1,46 @@
+/// [StringsManager] text in english is the same key for translation map later
+library;
+
+class StringsManager {
+  /// -------------------------------belong to filters---------------------------------------->
+  static const String noFilter = "No Filter";
+  static const String purple = "Purple";
+  static const String yellow = "Yellow";
+  static const String cyan = "Cyan";
+  static const String bw = "B&W";
+  static const String oldTimes = "Old Times";
+  static const String coldLife = "Cold Life";
+  static const String sepium = "Sepium";
+  static const String milk = "Milk";
+  static const String brightness = "Brightness";
+  static const String contrast = "Contrast";
+  static const String saturation = "Saturation";
+  static const String warmth = "Warmth";
+  static const String vignette = "Vignette";
+  static const String sharpen = "Sharpen";
+  static const String bwRedChannel = "B&W Red Channel";
+  static const String sepiaTone = "Sepia Tone";
+  static const String haze = "Haze";
+  static const String crimson = "Crimson";
+  static const String tealOrange = "Teal & Orange";
+  static const String darken = "Darken";
+  static const String brighten = "Brighten";
+  static const String popArt = "Pop Art";
+  static const String bwHighContrast = "B&W High Contrast";
+  static const String cool = "Cool";
+  static const String vintage = "Vintage";
+  static const String glamour = "Glamour";
+  static const String lomo = "Lomo";
+  static const String dramatic = "Dramatic";
+  static const String soft = "Soft";
+  static const String rioDeJaneiro = "Rio de Janeiro";
+  static const String tokyo = "Tokyo";
+
+  /// ----------------------------------------------------------------------->
+
+  static const String loading = 'Loading...';
+  static const String processing = 'Processing...';
+  static const String done = 'Done';
+  static const String apply = 'Apply';
+
+}
